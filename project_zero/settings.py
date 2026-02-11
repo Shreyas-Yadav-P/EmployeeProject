bf003fe51cf6831780020e7ff8f3d01b246ee1da
Shreyas-Yadav-P <eng25ct0054@dsu.edu.in>
2026-02-11 22:11:56 +0530

    Final Deployment settings with debug False

--- project_zero/settings.py
+++ project_zero/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)994_mi!b)wn8gsd)#3-*k5f4uobm@+g3^pq^by-juyf#!(e4s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
