b16c9c95802a730e41fd549341fc6f9b41381394
Shreyas-Yadav-P <eng25ct0054@dsu.edu.in>
2026-02-11 23:34:42 +0530

    Last verification

--- django_basic/urls.py
+++ django_basic/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project_zero project.
+URL configuration for django_basic project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
