f954654d6e4a18c7488d5bf7368672cf8ce13e5f
Shreyas-Yadav-P <eng25ct0054@dsu.edu.in>
2026-02-11 22:39:24 +0530

    Added missing employee_detail view

--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,11 @@
 
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from employees.models import Employee
 from .serializers import EmployeeSerializer
+
 
 @api_view(['GET'])
 def employee_api_list(request):
@@ -11,3 +13,10 @@ def employee_api_list(request):
     serializer = EmployeeSerializer(employees, many=True)
     return Response(serializer.data)
 
+
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        'employee': employee,
+    }
+    return render(request, 'employee_detail.html', context)
